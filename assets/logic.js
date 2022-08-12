1f8e637c1686947a10340afb5195d4e9b60aa38e
Victor Shaviya <vs610642@digicelgroup.com>
2022-08-11 20:04:16 -0500

    Create the fetch function for consuming the API

--- assets/logic.js
+++ assets/logic.js
@@ -5,3 +5,15 @@ var item = cardTemplate.contentEditable.cloneNode(true)
 for (let i = 0; i < 10; i++) {
   grid.append(item)
 }
+
+fetch("https://jsonplaceholder.typicode.com/posts")
+  .then(res => res.json())
+  .then(posts => {
+    grid.innerHTML = ''
+    posts.forEach(post => {
+      // const div = cardTemplate.contentEditable.cloneNode(true)
+      item.querySelector('[data-title]').textContent = post.title
+      item.querySelector('[data-body]').textContent = post.body
+      
+    });
+  })
