63acfbf816d5373cbe66fd46a5e2b36612dd43a4
Victor Shaviya <vs610642@digicelgroup.com>
2022-08-11 19:55:13 -0500

    Add a for loop for the templates

--- assets/logic.js
+++ assets/logic.js
@@ -2,3 +2,6 @@ const grid = document.querySelector('.grid');
 var cardTemplate = document.getElementById('card-template');
 var item = cardTemplate.contentEditable.cloneNode(true)
 
+for (let i = 0; i < 10; i++) {
+  grid.append(item)
+}
